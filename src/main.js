e4eb1f0d87e121c1ae06352b7b40dcdf98992364
weibowen <weibowen@gazrey.com>
2020-12-02 18:37:43 +0800

    修改gitignore

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import less from 'less'
 import router from './router/index'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 Vue.use(less)
 Vue.use(ElementUI)
