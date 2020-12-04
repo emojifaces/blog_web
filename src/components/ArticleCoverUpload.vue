67a68ea6602e7740b749735557b0f54d0e8c3633
wbw <1079980583>
2020-12-05 00:37:54 +0800

    明天加班写代码

--- src/components/ArticleCoverUpload.vue
+++ src/components/ArticleCoverUpload.vue
@@ -3,6 +3,10 @@
         <div class="pic">
             <img :src="imgUrl" alt="文章封面">
         </div>
+        <div class="pic-cover">
+            <div class="el-icon-picture-outline"></div>
+            设置文章封面
+        </div>
     </div>
 </template>
 <script>
@@ -18,12 +22,21 @@ export default {
 .articleCoverUpload-container{
     width: 300px;
     height: 170px;
+    position: relative;
     .pic{
         img{
             width: 300px;
             height: 170px;
             object-fit: cover;
         }
+    }
+    .pic-cover{
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background-color: darkgrey;
+        top: 0;
+        opacity: 0.5;
     }
 }
 </style>
